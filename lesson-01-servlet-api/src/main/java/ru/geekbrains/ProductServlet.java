e7fddf2b105bd1236d1632f4c59f73d242c62214
eutyupin <eu-1982@yandex.ru>
2021-12-22 15:47:52 +0700

    HW1

--- lesson-01-servlet-api/src/main/java/ru/geekbrains/ProductServlet.java
+++ lesson-01-servlet-api/src/main/java/ru/geekbrains/ProductServlet.java
@@ -25,7 +25,11 @@ public class ProductServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter wr = resp.getWriter();
         if (req.getParameter("id") != null) {
-            // TODO
+            Product product = productRepository.findById(Long.parseLong(req.getParameter("id")));
+            wr.println("PRODUCT ID: " + product.getId());
+            wr.println("<br>");
+            wr.println("PRODUCT NAME: " + product.getName());
+
         } else if (req.getPathInfo() == null || req.getPathInfo().equals("/")) {
             wr.println("<table>");
 
@@ -37,11 +41,10 @@ public class ProductServlet extends HttpServlet {
             for (Product product : productRepository.findAll()) {
                 wr.println("<tr>");
                 wr.println("<td>" + product.getId() + "</td>");
-                wr.println("<td>" + product.getName() + "</td>");
-                // TODO <a href='product?id=12'></a>
+                wr.println("<td>" + "<a href='product?id=" + product.getId().toString() +"'>"
+                        + product.getName() + "</a>" + "</td>");
                 wr.println("</tr>");
             }
-
             wr.println("</table>");
         }
     }
