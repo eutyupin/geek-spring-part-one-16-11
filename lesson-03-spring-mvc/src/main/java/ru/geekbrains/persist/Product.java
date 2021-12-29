81fab389ce82786000ec866eb4b86142290a33c7
eu-1982@yandex.ru <100251793125Jeka?>
2021-12-29 23:53:42 +0700

    HW3

--- lesson-03-spring-mvc/src/main/java/ru/geekbrains/persist/Product.java
+++ lesson-03-spring-mvc/src/main/java/ru/geekbrains/persist/Product.java
@@ -8,10 +8,8 @@ public class Product {
 
     private String name;
 
-    // TODO
     private String description;
 
-    // TODO
     private BigDecimal price;
 
     public Product() {
@@ -36,5 +34,21 @@ public class Product {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
     }
 }
