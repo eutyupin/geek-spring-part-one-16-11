87390ed8356438884782e1727b6650d1835e3ca9
eu-1982@yandex.ru <100251793125Jeka?>
2021-12-30 09:41:27 +0700

    lesson4

---
+++ lesson-04-spring-boot/src/main/java/ru/geekbrains/persist/Product.java
@@ -0,0 +1,54 @@
+package ru.geekbrains.persist;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
