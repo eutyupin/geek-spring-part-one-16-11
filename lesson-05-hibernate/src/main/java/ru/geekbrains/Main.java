617c5364c04c05e79083cabbae8f80dc32330772
eu-1982@yandex.ru <100251793125Jeka?>
2022-01-12 13:33:31 +0700

    lesson5

--- lesson-05-hibernate/src/main/java/ru/geekbrains/Main.java
+++ lesson-05-hibernate/src/main/java/ru/geekbrains/Main.java
@@ -1,7 +1,26 @@
 package ru.geekbrains;
 
+import org.hibernate.cfg.Configuration;
+import ru.geekbrains.entity.Product;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
+        EntityManagerFactory emFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+        EntityManager em = emFactory.createEntityManager();
+        ProductDao productOperations = new ProductDao(em);
+        for (Product product : productOperations.findAll()) {
+            System.out.println(product);
+        }
+        // some methods from productOperations
 
+        em.close();
     }
+
+
 }
