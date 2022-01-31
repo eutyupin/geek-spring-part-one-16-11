bf2fc284cd468129a7ac0c46d7800f8e061c6754
eu-1982@yandex.ru <100251793125Jeka?>
2022-01-31 20:40:50 +0700

    HW9

--- lesson-09-REST1/src/main/java/ru/geekbrains/persist/CategoryRepository.java
+++ lesson-09-REST1/src/main/java/ru/geekbrains/persist/CategoryRepository.java
@@ -1,6 +1,10 @@
 package ru.geekbrains.persist;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
 }
